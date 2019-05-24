506af4c3be2d7948ded2e66cd7e9807bd98a208b
mihseh <mihseh@ipac.caltech.edu>
2019-05-24 16:21:43 -0700

    change pykoa structure for packaging

--- pykoa/setup.py
+++ pykoa/setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 extensions = []
 
-reqs = ['astropy', 'requests']
+reqs = ['astropy', 'requests', 'xmltodict']
 
 with open ("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="pykoa",
-    version="0.1.2",
+    version="0.1.5",
     author="Mihseh Kong",
     description="KOA archive access client", 
     long_description = long_description,
@@ -20,7 +20,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Topic :: Scientific/Engineering :: Astronomy'],
-    packages=find_packages(),
+    packages=['pykoa', 'pykoa/koa'],
     data_files=[],
     install_requires=reqs,
     python_requires='>= 3.6',
